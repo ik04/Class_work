9afde0a3210d0a52883f50f7f6e05ca8aa166d53
ik04 <ishaankhurana123@gmail.com>
2023-10-27 14:05:22 +0530

    feat: add readme for week 11, refactor week 11

--- OS/week10/Q10.c
+++ OS/week10/Q10.c
@@ -1,92 +1,95 @@
-#include<stdio.h>
+#include <stdio.h>
+int main()
+{
+    // P0, P1, P2, P3, P4 are the Process names here
+ 
+    int n, m, i, j, k;
 
-int main() {
-  /* array will store at most 5 process with 3 resoures if your process or 
-  resources is greater than 5 and 3 then increase the size of array */
-  int p, c, count = 0, i, j, alc[5][3], max[5][3], need[5][3], safe[5], available[3], done[5], terminate = 0;
-  printf("Enter the number of process and resources");
-  scanf("%d %d", & p, & c);
-  // p is process and c is diffrent resources 
-  printf("enter allocation of resource of all process %dx%d matrix", p, c);
-  for (i = 0; i < p; i++) {
-    for (j = 0; j < c; j++) {
-      scanf("%d", & alc[i][j]);
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+    printf("Enter number of resources: ");
+    scanf("%d",&m);
+    printf("Enter allocation of each process\n:");
+    int alloc[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("\nalloc[%d][%d]: ",i+1,j+1);
+            scanf("%d",&alloc[i][j]);
     }
-  }
-  printf("enter the max resource process required %dx%d matrix", p, c);
-  for (i = 0; i < p; i++) {
-    for (j = 0; j < c; j++) {
-      scanf("%d", & max[i][j]);
+}
+   
+    int max[n][m];
+    printf("Enter maximum of each problem\n:");
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            printf("\nmax[%d][%d]: ",i+1,j+1);
+            scanf("%d",&max[i][j]);
     }
-  }
-  printf("enter the  available resource");
-  for (i = 0; i < c; i++)
-    scanf("%d", & available[i]);
-
-  printf("\n need resources matrix are\n");
-  for (i = 0; i < p; i++) {
-    for (j = 0; j < c; j++) {
-      need[i][j] = max[i][j] - alc[i][j];
-      printf("%d\t", need[i][j]);
-    }
-    printf("\n");
-  }
-  /* once process execute variable done will stop them for again execution */
-  for (i = 0; i < p; i++) {
-    done[i] = 0;
-  }
-  while (count < p) {
-    for (i = 0; i < p; i++) {
-      if (done[i] == 0) {
-        for (j = 0; j < c; j++) {
-          if (need[i][j] > available[j])
-            break;
-        }
-        //when need matrix is not greater then available matrix then if j==c will true
-        if (j == c) {
-          safe[count] = i;
-          done[i] = 1;
-          /* now process get execute release the resources and add them in available resources */
-          for (j = 0; j < c; j++) {
-            available[j] += alc[i][j];
-          }
-          count++;
-          terminate = 0;
-        } else {
-          terminate++;
-        }
-      }
-    }
-    if (terminate == (p - 1)) {
-      printf("safe sequence does not exist");
-      break;
-    }
-
-  }
-  if (terminate != (p - 1)) {
-    printf("\n available resource after completion\n");
-    for (i = 0; i < c; i++) {
-      printf("%d\t", available[i]);
-    }
-    printf("\n safe sequence are\n");
-    for (i = 0; i < p; i++) {
-      printf("p%d\t", safe[i]);
-    }
-  }
-
-  return 0;
+}
+        int avail[m];
+    printf("Enter the available resources: ");
+    for(int i=0;i<m;i++){
+        printf("avail[%d]:\n ",i+1);
+        scanf("%d",&avail[i]);
 }
 
 
-/*
 
-3 3
-0 1 0
-2 0 0
-3 0 2
-7 5 3
-3 2 2
-9 0 2
-3 3 2
 
-*/
+ 
+    int f[n], ans[n], ind = 0;
+    for (k = 0; k < n; k++) {
+        f[k] = 0;
+    }
+    int need[n][m];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+    }
+    int y = 0;
+    for (k = 0; k < 5; k++) {
+        for (i = 0; i < n; i++) {
+            if (f[i] == 0) {
+ 
+                int flag = 0;
+                for (j = 0; j < m; j++) {
+                    if (need[i][j] > avail[j]){
+                        flag = 1;
+                         break;
+                    }
+                }
+ 
+                if (flag == 0) {
+                    ans[ind++] = i;
+                    for (y = 0; y < m; y++)
+                        avail[y] += alloc[i][y];
+                    f[i] = 1;
+                }
+            }
+        }
+    }
+   
+      int flag = 1;
+       
+      for(int i=0;i<n;i++)
+    {
+      if(f[i]==0)
+      {
+        flag=0;
+         printf("The following system is not safe");
+        break;
+      }
+    }
+     
+      if(flag==1)
+    {
+      printf("Following is the SAFE Sequence\n");
+      for (i = 0; i < n - 1; i++)
+        printf(" P%d ->", ans[i]);
+      printf(" P%d", ans[n - 1]);
+    }
+     
+ 
+    return (0);
+ 
+}
--- OS/week11/Q11.c
+++ OS/week11/Q11.c
@@ -1,85 +1,144 @@
 #include <stdio.h>
+#include <limits.h>
 
-#define MAX_BLOCKS 5
-#define MAX_PROCESSES 4
+#define MAX_BLOCKS 100
 
-int memory_blocks[MAX_BLOCKS] = {100, 500, 200, 300, 600};
-int processes[MAX_PROCESSES] = {212, 417, 112, 426};
-int allocation[MAX_PROCESSES];
+// Structure to represent a memory block
+struct MemoryBlock {
+    int size;
+    int allocated;  // 0 for unallocated, 1 for allocated
+};
 
-void first_fit() {
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        for (int j = 0; j < MAX_BLOCKS; ++j) {
-            if (memory_blocks[j] >= processes[i]) {
-                allocation[i] = j;
-                memory_blocks[j] -= processes[i];
-                break;  // Break the loop after allocating to the first suitable block
-            }
-        }
-    }
-
-    // Print allocation
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        printf("Process %d allocated to Block %d\n", i + 1, allocation[i] + 1);
-    }
-}
-    
-
-void best_fit() {
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        int best_fit_index = -1;
-        for (int j = 0; j < MAX_BLOCKS; ++j) {
-            if (memory_blocks[j] >= processes[i]) {
-                if (best_fit_index == -1 || memory_blocks[j] < memory_blocks[best_fit_index]) {
-                    best_fit_index = j;
-                }
-            }
-        }
-
-        if (best_fit_index != -1) {
-            allocation[i] = best_fit_index;
-            memory_blocks[best_fit_index] -= processes[i];
-        }
-    }
-
-    // Print allocation
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        printf("Process %d allocated to Block %d\n", i + 1, allocation[i] + 1);
-    }
-}
-
-void worst_fit() {
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        int worst_fit_index = -1;
-        for (int j = 0; j < MAX_BLOCKS; ++j) {
-            if (memory_blocks[j] >= processes[i]) {
-                if (worst_fit_index == -1 || memory_blocks[j] > memory_blocks[worst_fit_index]) {
-                    worst_fit_index = j;
-                }
-            }
-        }
-
-        if (worst_fit_index != -1) {
-            allocation[i] = worst_fit_index;
-            memory_blocks[worst_fit_index] -= processes[i];
-        }
-    }
-
-    // Print allocation
-    for (int i = 0; i < MAX_PROCESSES; ++i) {
-        printf("Process %d allocated to Block %d\n", i + 1, allocation[i] + 1);
-    }
-}
+// Function prototypes
+void initializeMemory(struct MemoryBlock memory[], int size);
+void displayMemory(struct MemoryBlock memory[], int size);
+int firstFit(struct MemoryBlock memory[], int size, int processSize);
+int bestFit(struct MemoryBlock memory[], int size, int processSize);
+int worstFit(struct MemoryBlock memory[], int size, int processSize);
 
 int main() {
-    printf("First Fit Allocation:\n");
-    first_fit();
+    struct MemoryBlock memory[MAX_BLOCKS];
+    int memorySize, processSize, choice, index;
 
-    printf("\nBest Fit Allocation:\n");
-    best_fit();
+    printf("Enter the total size of memory: ");
+    scanf("%d", &memorySize);
 
-    printf("\nWorst Fit Allocation:\n");
-    worst_fit();
+    initializeMemory(memory, memorySize);
+
+    do {
+        printf("\nMemory Allocation Techniques\n");
+        printf("1. First Fit\n");
+        printf("2. Best Fit\n");
+        printf("3. Worst Fit\n");
+        printf("4. Display Memory\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter process size: ");
+                scanf("%d", &processSize);
+                index = firstFit(memory, memorySize, processSize);
+                break;
+            case 2:
+                printf("Enter process size: ");
+                scanf("%d", &processSize);
+                index = bestFit(memory, memorySize, processSize);
+                break;
+            case 3:
+                printf("Enter process size: ");
+                scanf("%d", &processSize);
+                index = worstFit(memory, memorySize, processSize);
+                break;
+            case 4:
+                displayMemory(memory, memorySize);
+                break;
+            case 5:
+                printf("Exiting...\n");
+                break;
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+        }
+    } while (choice != 5);
 
     return 0;
+}
+
+// Function to initialize memory blocks
+void initializeMemory(struct MemoryBlock memory[], int size) {
+    for (int i = 0; i < size; i++) {
+        memory[i].size = 0;
+        memory[i].allocated = 0;
+    }
+}
+
+// Function to display the current state of memory
+void displayMemory(struct MemoryBlock memory[], int size) {
+    printf("\nMemory Status:\n");
+    for (int i = 0; i < size; i++) {
+        printf("Block %d: Size = %d, Allocated = %s\n", i + 1, memory[i].size,
+               memory[i].allocated ? "Yes" : "No");
+    }
+}
+
+// First Fit Algorithm
+int firstFit(struct MemoryBlock memory[], int size, int processSize) {
+    for (int i = 0; i < size; i++) {
+        if (!memory[i].allocated && memory[i].size >= processSize) {
+            memory[i].allocated = 1;
+            printf("Memory allocated at Block %d using First Fit.\n", i + 1);
+            return i;
+        }
+    }
+    printf("Memory allocation failed using First Fit.\n");
+    return -1;
+}
+
+// Best Fit Algorithm
+int bestFit(struct MemoryBlock memory[], int size, int processSize) {
+    int bestFitIndex = -1;
+    int bestFitSize = INT_MAX;
+
+    for (int i = 0; i < size; i++) {
+        if (!memory[i].allocated && memory[i].size >= processSize) {
+            if (memory[i].size < bestFitSize) {
+                bestFitSize = memory[i].size;
+                bestFitIndex = i;
+            }
+        }
+    }
+
+    if (bestFitIndex != -1) {
+        memory[bestFitIndex].allocated = 1;
+        printf("Memory allocated at Block %d using Best Fit.\n", bestFitIndex + 1);
+    } else {
+        printf("Memory allocation failed using Best Fit.\n");
+    }
+
+    return bestFitIndex;
+}
+
+// Worst Fit Algorithm
+int worstFit(struct MemoryBlock memory[], int size, int processSize) {
+    int worstFitIndex = -1;
+    int worstFitSize = -1;
+
+    for (int i = 0; i < size; i++) {
+        if (!memory[i].allocated && memory[i].size >= processSize) {
+            if (memory[i].size > worstFitSize) {
+                worstFitSize = memory[i].size;
+                worstFitIndex = i;
+            }
+        }
+    }
+
+    if (worstFitIndex != -1) {
+        memory[worstFitIndex].allocated = 1;
+        printf("Memory allocated at Block %d using Worst Fit.\n", worstFitIndex + 1);
+    } else {
+        printf("Memory allocation failed using Worst Fit.\n");
+    }
+
+    return worstFitIndex;
 }
